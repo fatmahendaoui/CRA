7b21c9f9e0ef8a2242921c0924a91cd2d3d16b92
SarraBouzidi <sarra.bouzidi@isamm.u-manouba.tn>
2024-04-08 09:43:28 +0100

    Ajouter une interface similaire à celle de Figma pour l'UI/UX, créer des scénarios comme dans Figma, intégrer la fonctionnalité de traduction, et implémenter la vérification de la liste en fonction de l'idDomaine.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,15 @@ import { HasDomaineGuard } from './guards/has-domaine.guard';
 import { ProjectService } from './features/projects/services/projects.service';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
   imports: [RouterOutlet,
-    MatDatepickerModule, MatNativeDateModule], 
+    MatDatepickerModule, MatNativeDateModule,
+  MatTooltipModule], 
    providers: [
     ProjectService
   ],
