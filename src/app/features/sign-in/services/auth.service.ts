8cb1934a0e8233f277d92a8dfd340cf5e5cd2688
unknown <SarraBouzidi>
2024-11-07 13:11:49 +0100

    office

--- src/app/features/sign-in/services/auth.service.ts
+++ src/app/features/sign-in/services/auth.service.ts
@@ -168,19 +168,21 @@ export class AuthService {
   }
   async loginWithMicrosoft(): Promise<void> {
     try {
-      const provider = new OAuthProvider('microsoft.com');
+      const provider = new OAuthProvider('google.com');
       const userCredential = await signInWithPopup(this.auth, provider);
       const user = userCredential.user;
       console.log('User logged in with Microsoft:', user);
 
       const exists = await this.CheckUserExist(user.uid);
       if (exists) {
+        console.log('User logged in with Microsoft:', user);
         this.router.navigate(['/timesheet/' + this.auth.currentUser?.uid + '/' + new Date()]);
         //this.router.navigate(['/dashbord']);
       } else {
         this.router.navigate(['create-domaine']);
       }
     } catch (error) {
+      console.log('befff');
       console.error('Error logging in with Microsoft:', error);
       throw error;
     }
